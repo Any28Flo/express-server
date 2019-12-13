5661f6426a9bb2dabc899a09eb09b0d55462c4f8
Analin Flores <foglzerika@gmail.com>
2019-12-13 17:27:51 -0600

    Adding images and styles

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require ('express');
 //We create our own server named app
 //Express server handling requests and responses
 const app = express();
+//Make everything inside of public/available
+app.use(express.static('public'));
 
 //our first route
 
-app.get('/' , (req, res) =>{
+app.get('/' , (req, res, next) =>{
     console.log(req);
     res.send('<h1>Welcome ironhacker</h1>');
 
